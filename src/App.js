d06602d588abc2ad12a0ede3b099acf076597e46
teyssedou <mikaelteyssedou@gmail.com>
2018-05-25 15:13:30 +0200

    create icon on map

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 import Map from "./map.js";
 import Menu from "./menu.js";
 
--- src/souscat.js
+++ src/souscat.js
@@ -1,51 +1,84 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { render } from "react-dom";
 import Datas from "./places.json";
 import Plus from "./img/plus.svg";
+import Parking from "./img/parking.svg";
+import Bus from "./img/bus.svg";
+import Primaire from "./img/primaire.svg";
+import Maternelle from "./img/maternelle.svg";
+import Verre from "./img/verre.svg";
+import Dechetterie from "./img/dechetterie.svg";
+
+var listImg = {
+  parking: Parking,
+  bus: Bus,
+  primaire: Primaire,
+  maternelle: Maternelle,
+  verre: Verre,
+  dechetterie: Dechetterie
+};
 
 class Souscat extends React.Component {
-
   displayCat(i) {
-   var list = document.getElementById("list" + i);
+    var list = document.getElementById("list" + i);
 
-   if (list.classList.contains("display-none")) {
-     list.classList.remove("display-none");
-   } else {
-     list.classList.add("display-none");
-   }
- }
+    if (list.classList.contains("display-none")) {
+      list.classList.remove("display-none");
+    } else {
+      list.classList.add("display-none");
+    }
+  }
 
   render() {
-      return (
-      	<div>
-          {Datas.map((data, i) =>
-				    <div key={data.id}>
-              <div className="title">
-                <img src={Plus} id="iconTitle" />
-	              <h5 id={i} onClick={()=>this.displayCat(i)}>{data.name}</h5>
-              </div>
-	      		  <ul id={"list"+ i} className="display-none">
-				      {data.children.map((child, j) =>
-                <li key={child.id} className="sousCatName">{child.name}
-                <ul className="display-none">
-                   {child.places.map((place, k) => (
-                     <li>
-                       {place.name}
-                       <br />
-                       {place.lat}
-                       <br />
-                       {place.lon}
-                     </li>
-                   ))}
-                 </ul></li>)}
-				      </ul>
-				    </div>
-	        )}
-        </div>
-      );
+    console.log({ Datas });
+
+    const position = (
+      <div>
+        {Datas.map((data, i) => (
+          <div key={data.id}>
+            <h5 id={i} onClick={() => this.displayCat(i)}>
+              {data.name}
+            </h5>
+            <ul id={"list" + i} className="display-none">
+              {data.children.map((child, j) => (
+                <li key={child.id}>{child.name}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+    );
+
+    return (
+      <div>{position}</div>
+
+      /* <div>
+        {Datas.map((data, i) => (
+          <div key={data.id}>
+            <h5 id={i} onClick={() => this.displayCat(i)}>
+              {data.name}
+            </h5>
+            <ul id={"list" + i} className="display-none">
+              {data.children.map((child, j) => (
+                <li key={child.id}>
+                  {child.name}
+                  {child.places.map((place, k) => (
+                    <li>
+                      {place.name}
+                      <br />
+                      {place.lat}
+                      <br />
+                      {place.lon}
+                    </li>
+                  ))}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div> */
+    );
   }
 }
 
 export default Souscat;
-
-
